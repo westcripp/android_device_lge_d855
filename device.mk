523a4e186be5846f50614d47cc7843d4ad0bd1b7
westcripp <korukaltan@gmail.com>
2014-12-03 23:30:20 +0200

    new merges
    Change-Id: I4e1f7ac214f180fbeedcc5fcab9bed2359bbf1a7

--- device.mk
+++ device.mk
@@ -19,6 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lge/d855/d855-vendor.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    device/lge/g3-common/configs/mixer_paths_qcwcn.xml:system/etc/mixer_paths.xml
+	
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
@@ -27,12 +32,9 @@ PRODUCT_PACKAGES += \
     init.galbi.bt.sh \
     init.galbi.bt_vendor.rc
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
-
 # Wifi
 PRODUCT_PACKAGES += \
+    hostapd_default.conf \
     libwcnss_qmi \
     wcnss_service
 
